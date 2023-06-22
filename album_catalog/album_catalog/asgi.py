9271886a42fc56418a2a0eb8e884122ea44b319e
Atanas Kostov <ankostov@gmail.com>
2023-06-22 21:29:16 +0300

    template

--- album_catalog/album_catalog/asgi.py
+++ album_catalog/album_catalog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for albun_catalog project.
+ASGI config for album_catalog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'albun_catalog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'album_catalog.settings')
 
 application = get_asgi_application()
